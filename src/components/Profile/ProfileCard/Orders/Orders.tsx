585d5aac085b758cdf92435b59bdfbe6a8cf0b89
Davian Canty <davian49@hotmail.com>
2023-02-19 18:24:47 -0500

    started previousOrders

--- src/components/Profile/ProfileCard/Orders/Orders.tsx
+++ src/components/Profile/ProfileCard/Orders/Orders.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { ICart } from '../../../../models/ICart';
+import { IProduct } from '../../../../models/IProduct';
+import { remoteUrl } from '../../../../models/URL';
 
 const Orders: React.FC = () => {
+    const [orders, setOrders] = useState<IProduct[]>([]);
+    useEffect(() => {
+        const getOrders = async () => {
+            const token = localStorage.getItem("token");
+            const config = {
+                headers: { Authorization: `Bearer ${token}` },
+            };
+            const result = await axios.get(`${remoteUrl}/orders/`, config)
+            setOrders(result.data.Items)
+            console.log("result.entries", result.data);
+        }
+        getOrders()
+        console.log(orders)
+        orders.forEach((item) => (console.log(item)))
+    }, [])
+
+    
+
     return (
-        <div>Orders</div>
-    );
+        <div className="productContainer">
+
+        </div>
+    )
 }
 
 export default Orders;

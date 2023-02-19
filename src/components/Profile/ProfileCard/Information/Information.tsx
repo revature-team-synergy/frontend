10e9d594e4ae1cf997dd56f99f75b5566d6ece1c
Christopher Crawford <chriscrawford360@outlook.com>
2023-02-19 12:13:27 -0600

    Add User Update Information

--- src/components/Profile/ProfileCard/Information/Information.tsx
+++ src/components/Profile/ProfileCard/Information/Information.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from "react-redux";
 import { RootState } from "../../../../redux/Store";
 import profilePic from "../../../../assets/profile-picture.jpg";
 import "./Information.css";
+import { User } from "../../../../models/User";
 
 const Information: React.FC = () => {
 
     const userState = useSelector((state: RootState) => state.user);
+    const [user, setUser] = useState<User>(userState.user);
 
     return (
         <div className="profileCardContainer">
@@ -17,10 +19,28 @@ const Information: React.FC = () => {
             <form className="profileInfo" onSubmit={(e) => {e.preventDefault()}}>
                 <div id="nameContainer">
                     <label htmlFor="firstName">First Name</label>
-                    <input type="text" name="firstName" id="firstName" value={userState.user.firstName} readOnly/>
+                    <input type="text" name="firstName" id="firstName" 
+                    value={user.firstName} 
+                    onChange={(e) => setUser({
+                        firstName: e.target.value,
+                        id: user.id,
+                        lastName: user.lastName,
+                        password: user.password,
+                        email: user.email,
+                        userRole: user.userRole
+                    })}/>
 
                     <label htmlFor="lastName">Last Name</label>
-                    <input type="text" name="lastName" id="lastName" value={userState.user.lastName} readOnly/>
+                    <input type="text" name="lastName" id="lastName" 
+                    value={user.lastName}
+                    onChange={(e) => setUser({
+                        lastName: e.target.value,
+                        id: user.id,
+                        firstName: user.firstName,
+                        password: user.password,
+                        email: user.email,
+                        userRole: user.userRole
+                    })}/>
                 </div>
 
                 <label htmlFor="email">Email</label>
